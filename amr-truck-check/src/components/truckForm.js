30b6cdab5e9d113299a56a5a871ec44cded1956b
paulraucci <54955537+paulraucci@users.noreply.github.com>
2020-01-15 17:36:41 -0500

    some components created

---
+++ amr-truck-check/src/components/truckForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class truckForm extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default truckForm;
